b1d594cdf108fd3f65c6172fd9388696a68998e4
Emir <luchtdev@gmail.com>
2026-09-17 00:14:56 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1911,6 +1911,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host=os.getenv("HOST", "0.0.0.0"),
-        port=int(os.getenv("PORT", "8000")),
+        port=int(os.getenv("PORT", "8001")),
         reload=False,
     )
